f4de0ce2358b2c958de36a51e342cee0663f746a
ale rimoldi <ale@graphicslab.org>
2018-12-18 14:16:58 +0100

    handling the actions

--- sync/index.php
+++ sync/index.php
@@ -10,19 +10,32 @@ $hook_secret = file_get_contents('secret.txt');
 print(">".trim($hook_secret)."<");
 
 if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
-	throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
+    throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
 }
 
 list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
 
 $raw_post = file_get_contents('php://input');
 if (hash_equals($hash, hash_hmac($algo, $raw_post, $hook_secret))) {
-	throw new \Exception('Hook secret does not match.');
+    throw new \Exception('Hook secret does not match.');
 }
 
 $json = $rawPost ?: file_get_contents('php://input');
 
 $payload = json_decode($json);
 
-// shell_exec('cd  user/pages/ && git reset --hard HEAD && git pull' );
 
+switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
+    case 'ping':
+        echo 'pong';
+        break;
+    // case 'push':
+        // shell_exec('cd  user/pages/ && git reset --hard HEAD && git pull' );
+    case 'create':
+        break;
+    default:
+        header('HTTP/1.0 404 Not Found');
+        echo "Event:$_SERVER[HTTP_X_GITHUB_EVENT] Payload:\n";
+        print_r($payload); # For debug only. Can be found in GitHub hook log.
+        die();
+}
